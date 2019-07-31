ab23309dfce12acfd00bdc6c22b2696e78d8b12b
dapplion <guillem.lion.benosa@gmail.com>
2019-07-31 19:51:21 +0200

    Updates

---
+++ precache-manifest.23c303303147e64e86ad760375b6c629.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fd4c080d34b55c3b8b040917af1935f",
+    "url": "/augmented-tbc-design/index.html"
+  },
+  {
+    "revision": "b625b669a41bc1dd357b",
+    "url": "/augmented-tbc-design/static/css/main.31240ce3.chunk.css"
+  },
+  {
+    "revision": "2f9c9320df13e4e5c22b",
+    "url": "/augmented-tbc-design/static/js/2.ab415a41.chunk.js"
+  },
+  {
+    "revision": "b625b669a41bc1dd357b",
+    "url": "/augmented-tbc-design/static/js/main.58438057.chunk.js"
+  },
+  {
+    "revision": "707b94664485cbbcc81b",
+    "url": "/augmented-tbc-design/static/js/runtime~main.1a6ed195.js"
+  }
+]);
